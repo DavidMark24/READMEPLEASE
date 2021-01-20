9707a51cb58967308ebb9fb34202a8014edec9fc
David Johnson <david.m.johnson32@gmail.com>
2021-01-19 20:41:05 -0800

    Added questions and init function

--- index.js
+++ index.js
@@ -1,8 +1,81 @@
+// Global Variables
+
 const inquirer = require('inquirer')
 const fs = require('fs')
-const axios = require('axios')
 const markdown = require('./utils/generateMarkdown')
 
-inquirer.prompt(
-    
-)
+// Inquirer Questions
+
+const questions = [
+    {
+    type: "input",
+    name: "title",
+    message:"What is the title of your project?"
+    },
+    {
+    type: "input",
+    name: "description" ,
+    message: "Please provide your project's description"
+    },
+    {
+    type: "input",
+    name: "installation",
+    message: "Please provide the installation instructions"
+    },
+    {
+    type: "input",
+    name: "usage",
+     message: "Please provide the project usage"
+    },
+    {
+    type: "list",
+    name: "licence",
+    message: "Please provide the project licence",
+    choices:["Apache license 2.0', 'Creative Commons license family', 'MIT', 'Mozilla Public License 2.0"]
+    },
+    {
+    type: "input",
+    name: "contributing",
+    message: "Please provide the contributing parties"
+    },
+    {
+    type: "input",
+    name: "test",
+    message: "Please provide the project tests"
+    },
+    {
+    type: "input",
+    name: "username",
+    message: "What is your github user name?"
+    },
+    {
+    type: "input",
+    name: "email",
+    message: "What is your email?"
+    },
+];
+
+// Function to initialize progam
+
+function init() {
+    inquirer.prompt(questions).then(answers => {
+        console.log("Answers", answers);
+
+        const markdown = generateMarkdown(answers);
+
+        fs.writeFile('README.md', markdown, () => {console.log("Poof! README successful!)");});
+    })
+}
+
+// Call function
+
+init();
+
+
+
+
+
+
+
+
+
